3dbcd64814d98c8d4722856c03149989a78ba874
Isaac Svedin <swedinisaac@gmail.com>
2025-04-02 13:34:28 +0200

    Fixed the error and added more comments.

--- server.js
+++ server.js
@@ -40,19 +40,32 @@ app.get("/", async (req, res) => {
     });
 });
 
+/**
+ * Raderar vald kurs.
+ */
 app.post("/", async (req, res) => {
     const id = req.body.courseid;
-    console.log(id);
-    const result = await db_client.query(`
-        DELETE FROM Courses WHERE courseID = $1;
-    `, [id]);
-    res.redirect("/");
+    try {
+        const result = await db_client.query(`
+            DELETE FROM Courses WHERE courseID = $1;
+        `, [id]);
+        res.redirect("/");
+    } catch (error) {
+        console.error(error);
+    }
 });
 
+/**
+ * Ser till att formData inte är undefined när användaren går in på courseform.
+ */
 app.get("/courseform", async (req, res) => {
     res.render("courseform", { errors: [], formData: { code: "", name: "", progression: "", syllabus: "" } });
 });
 
+/**
+ * Validerar inmatningsfält och därefter lägger till en ny kurs i tabellen Courses.
+ * Om ett inmatningsfält har fel i sig, laddas sidan om, fyller i fälten och lägger till tydliga felmeddelanden.
+ */
 app.post("/courseform", async (req, res) => {
     const formData = {
         code: req.body.coursecode,
@@ -95,12 +108,16 @@ app.post("/courseform", async (req, res) => {
     // Om den inte är tom, skickas inte informationen utan den returnerar felmeddelanden och den inmatade informationen till formulärsidan.
     if (errors.length == 0) {
         const progressID = progressions.indexOf(formData.progression) + 1;
-        const result = await db_client.query(`
-            INSERT INTO Courses (courseCode, courseName, syllabus, progressID)
-            VALUES($1, $2, $3, $4)
-            `, [formData.code, formData.name, formData.syllabus, progressID]);
-        res.render("courseform", { errors: [], formData: { code: "", name: "", progression: "", syllabus: "" } });
-        res.redirect("/");
+        try {
+            const result = await db_client.query(`
+                INSERT INTO Courses (courseCode, courseName, syllabus, progressID)
+                VALUES($1, $2, $3, $4)
+                `, [formData.code, formData.name, formData.syllabus, progressID]);
+            return res.redirect("/");
+        } catch (error) {
+            console.error(error);
+            return res.render("courseform", { errors: [{name: "Fel på databasen", message: "Kunde inte lägga till ny kurs!"}], formData: formData });
+        }
     } else {
         return res.render("courseform", { errors: errors, formData: formData });
     }
@@ -199,79 +216,99 @@ async function isCodeUnique(code) {
     return result.rows.length != 0;
 }
 
-async function isCodeUnique2(code, id) {
-    const result = await db_client.query(`
-        SELECT courseCode FROM Courses WHERE courseCode = $1 AND courseID != $2;    
-    `, [code, id]);
-    console.log(result.rows.length !== 0);
-    return result.rows.length !== 0;
+/**
+ * Kollar om kurskoden är unik och att den tillåter att kurskoden kan vara samma för en och samma rad.
+ * Alltså den hamnar inte i en konflikt med sig själv.
+ * @param {string} code - kurskod
+ * @param {number} id - id på raden som är påväg att uppdateras.
+ * @returns {void}
+ */
+async function isCodeUniqueAndNotSameRow(code, id) {
+    try {
+        const result = await db_client.query(`
+            SELECT courseCode FROM Courses WHERE courseCode = $1 AND courseID != $2;    
+        `, [code, id]);
+        console.log(result.rows.length !== 0);
+        return result.rows.length !== 0;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
+/**
+ * Den skickar användaren till uppdateringsformuläret och fyller i uppgifterna i inmatningsfälten.
+ */
 app.post("/updateform", async (req, res) => {
     const progressions = ["A", "B", "C"];
-    if (req.body.formType === "go_to_update_form") {
-        const id = req.body.courseid;
+    const id = req.body.courseid;
+    try {
         const result = await db_client.query(`
             SELECT * FROM Courses WHERE courseid = $1;
         `, [id]);
+    const formData = {
+        id: id,
+        code: result.rows[0].coursecode,
+        name: result.rows[0].coursename,
+        progression: progressions[result.rows[0].progressid - 1],
+        syllabus: result.rows[0].syllabus
+    };
+    return res.render("updateform", { errors: [], formData: formData });
+    } catch (error) {
+        console.error(error);
+        return res.render("updateform", { errors: [{name: "Fel på databasen", message: "Kunde inte hämta kursinformation!"}], formData: formData });
+    }
+});
 
-        const formData = {
-            id: id,
-            code: result.rows[0].coursecode,
-            name: result.rows[0].coursename,
-            progression: progressions[result.rows[0].progressid - 1],
-            syllabus: result.rows[0].syllabus
-        };
-        console.log(formData);
-        res.render("updateform", { errors: [], formData: formData });
-    } else if (req.body.formType === "update_form") {
-        const formData = {
-            id: req.body.courseid,
-            code: req.body.coursecode,
-            name: req.body.coursename,
-            progression: req.body.progression,
-            syllabus: req.body.courseurl
-        };
-        const errors = [];
+app.post("/update", async (req, res) => {
+    const progressions = ["A", "B", "C"];
+    const formData = {
+        id: req.body.courseid,
+        code: req.body.coursecode,
+        name: req.body.coursename,
+        progression: req.body.progression,
+        syllabus: req.body.courseurl
+    };
+    const errors = [];
 
-        const fieldsEmpty = emptyFields(formData); // Lagrar tommafält error eller null vid inga tomma fält.
-        const tooManyCharsFields = tooManyCharacters(formData); // Lagrar för många tecken error eller null vid inga för många tecken.
+    const fieldsEmpty = emptyFields(formData); // Lagrar tommafält error eller null vid inga tomma fält.
+    const tooManyCharsFields = tooManyCharacters(formData); // Lagrar för många tecken error eller null vid inga för många tecken.
 
-        // Kollar om det finns errors lagrade.
-        if (fieldsEmpty) {
-            errors.push(fieldsEmpty);
-        }
-        if (tooManyCharsFields) {
-            errors.push(tooManyCharsFields);
-        }
-        
-        // Kollar om kurskoden är unik.
-        if (await isCodeUnique2(formData.code, formData.id)) {
-            errors.push({
-                name: "Kurskod är inte unikt",
-                message: `Kurskoder måste vara unika, en kurs med kurskoden ${formData.code} existrerar redan!`
-            });
-        }
-        // Kollar om det är ett korrekt värde i progression.
-        if (progressions.indexOf(formData.progression) === -1 && formData.progression != "") {
-            errors.push({
-                name: "Otillåtet värde i progression",
-                message: `Det otillåtna värdet, ${req.body.progression}, måste vara antigen A, B eller C!`
-            });
-        }
-        if (errors.length == 0) {
-            const progressID = progressions.indexOf(formData.progression) + 1;
-        
-            const result = await db_client.query(`
+    // Kollar om det finns errors lagrade.
+    if (fieldsEmpty) {
+        errors.push(fieldsEmpty);
+    }
+    if (tooManyCharsFields) {
+        errors.push(tooManyCharsFields);
+    }
+
+    // Kollar om kurskoden är unik.
+    if (await isCodeUniqueAndNotSameRow(formData.code, formData.id)) {
+        errors.push({
+            name: "Kurskod är inte unikt",
+            message: `Kurskoder måste vara unika, en kurs med kurskoden ${formData.code} existrerar redan!`
+        });
+    }
+    // Kollar om det är ett korrekt värde i progression.
+    if (progressions.indexOf(formData.progression) === -1 && formData.progression != "") {
+        errors.push({
+            name: "Otillåtet värde i progression",
+            message: `Det otillåtna värdet, ${req.body.progression}, måste vara antigen A, B eller C!`
+        });
+    }
+    if (errors.length == 0) {
+        const progressID = progressions.indexOf(formData.progression) + 1;
+        try {
+            await db_client.query(`
                 UPDATE Courses SET coursecode = $1, coursename = $2, progressID = $3, syllabus = $4 WHERE courseid = $5;
             `, [formData.code, formData.name, progressID, formData.syllabus, formData.id]);
-            res.redirect("/");
-            res.render("updateform", { errors: [], formData: { id: "", code: "", name: "", progression: "", syllabus: "" } });
-        } else {
-            return res.render("updateform", { errors: errors, formData: formData });
+            return res.redirect("/");
+        } catch (error) {
+            console.error(error);
+            return res.render("updateform", { errors: [{name: "Fel på databasen", message: "Kunde inte uppdatera kursen!"}], formData: formData });
         }
+        
     } else {
-        console.log("Stavade fel på formType i formuläret.");
+        return res.render("updateform", { errors: errors, formData: formData });
     }
 });
 
